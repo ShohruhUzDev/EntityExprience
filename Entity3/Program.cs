88fa89695000917a627be93d2fe2a3ad0bd0c202
ShohruhUzDev <shohruh7464784@mail.ru>
2021-05-05 23:15:53 +0500

    Crud operation

---
+++ Entity3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace Entity3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (StudenContext db = new StudenContext())
+            {
+                Student student1 = new Student() { Name = "shohruh", Age = 45 };
+                Student student2 = new Student() { Name = "shohzod", Age = 25 };
+
+                db.Students.AddRange(student1, student2);
+                db.SaveChanges();
+
+            }
+            using(StudenContext db=new StudenContext())
+            {
+                var students = db.Students.ToList();
+                foreach(var i in students)
+                {
+                    Console.WriteLine($"Ismi-{i.Name} yoshi={i.Age}");
+                }
+            }
+
+
+            //using (StudenContext db=new StudenContext())
+            //{
+
+            //}
+
+        }
+    }
+}
---
+++ Entity3/StudenContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity3
+{
+   public  class StudenContext:DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public StudenContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb; Database=studentdb; Trusted_Connection=True;");
+        }
+    }
+}
