4d3e0ba9f1649fa2e94f3f1b066387a93b15106e
ShohruhUzDev <shohruh7464784@mail.ru>
2021-05-05 19:06:29 +0500

    add to connect to database

---
+++ Entity/UserContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+   public class UserContext:DbContext
+    {
+        public DbSet<User> Users{ get; set; }
+
+        public UserContext()
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb; Database=userdb; Trusted_Connection=true;")
+        }
+    }
+}
