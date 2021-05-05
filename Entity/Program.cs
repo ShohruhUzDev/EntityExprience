e6995d6691a37a3b0970e6d106b224102d8c3080
ShohruhUzDev <shohruh7464784@mail.ru>
2021-05-05 19:11:05 +0500

    endi sinab kurish uchun asosiy kodni yozdik

--- Entity/Program.cs
+++ Entity/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Entity
 {
@@ -6,7 +7,23 @@ namespace Entity
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using(UserContext db=new UserContext())
+            {
+                User user1 = new User() { Name = "Shohruh", Age = 25 };
+                User user2 = new User() { Name = "Shohzod", Age = 20 };
+
+                db.Users.AddRange(user1, user2);
+                db.SaveChanges();
+                Console.WriteLine("dastur muvafaqiyatli ishladi");
+
+
+                var users = db.Users.ToList();
+                foreach(User i in users)
+                {
+                    Console.WriteLine($"ismi={i.Name}  yoshi={i.Age}");
+                }
+            }
+            Console.ReadLine();
         }
     }
 }
