5a6c27c98ce070d5f124a6aa7ebd9dd287ab4f5e
ShohruhUzDev <shohruh7464784@mail.ru>
2021-05-05 19:35:44 +0500

    tayyor database bilan ishlash uchun

---
+++ Entity2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace Entity2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           using(userdbContext _db=new userdbContext())
+            {
+                var users = _db.Users.ToList();
+                foreach(User i in users)
+                {
+                    Console.WriteLine($"Ismi={i.Name}  yoshi={i.Age}");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Entity2/userdbContext.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace Entity2
+{
+    public partial class userdbContext : DbContext
+    {
+        public userdbContext()
+        {
+        }
+
+        public userdbContext(DbContextOptions<userdbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb; Database=userdb; Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
