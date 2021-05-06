df61f63f3051da0df412015e446c27921553b077
ShohruhUzDev <shohruh7464784@mail.ru>
2021-05-06 11:41:59 +0500

    Add Migrations

---
+++ Entity3/Migrations/20210506063905_AddISMarried.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Entity3.Migrations
+{
+    public partial class AddISMarried : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsMarried",
+                table: "Students",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsMarried",
+                table: "Students");
+        }
+    }
+}
